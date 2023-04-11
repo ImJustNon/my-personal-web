ad5b2d992c175bce8cf116fc63b2a96c2c7d3bd1
ImJustNon <Im_Just_Non@hotmail.com>
2023-04-11 21:21:26 +0700

    rewrite server.js

--- server.js
+++ server.js
@@ -3,15 +3,28 @@ const config = require("./configs/config.js");
 const { normalizePort } = require("./utils/nornalizePort.js");
 const { onError } = require("./utils/portErrorHandler.js");
 
+
 const http = require("http");
 const chalk = require("chalk");
+const socket = require('socket.io');
+
+
+const server = http.createServer(app);
+const io = socket(server);
 
 
 const server_port = normalizePort(String(config.app.port));
 app.set("port", server_port);
 
 
-const server = http.createServer(app);
+// socketio
+io.on('connection', (socket) => {
+    console.log('[Socket.io] ✅ client connected ID : ' + socket.id);
+    socket.on('disconnect', () => {
+        console.log('[Socket.io] ⚠  client disconnected ID : ' + socket.id);
+    });
+});
+
 
 server.listen(server_port);
 
